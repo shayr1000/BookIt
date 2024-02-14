654a9ef34a41be1020a9c9e3ddecbafa5843a2b9
Shay Rotenberg <shayr1000@gmail.com>
2024-02-14 20:22:06 +0200

    add h2 text

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,7 @@ function App() {
         </a>
       </div>
       <h1>BookIt + React</h1>
+      <h2>2024</h2>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
